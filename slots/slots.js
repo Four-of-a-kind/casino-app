8c82c73b47ee13fd2cc7c596160ad8d7aa296ddb
Jaime Sanders <jaimelyn517@gmail.com>
2020-04-20 15:44:54 -0700

    pulls user profile from local storage and displays on nav

--- slots/slots.js
+++ slots/slots.js
@@ -1,1 +1,26 @@
-console.log('hi from the slots.js')
+// bring elements from DOM
+const userNameSpan = document.getElementById('user-name-span');
+const walletSpan = document.getElementById('wallet-span');
+const spinSpan = document.getElementById('spin-span');
+
+const spinButton = document.getElementById('spin-button');
+
+const user = JSON.parse(localStorage.getItem('USER'));
+
+// intialize spins
+let spinCounter = 5;
+spinSpan.textContent = spinCounter;
+
+loadUserProfile(user);
+
+
+
+
+
+
+
+// load user profile function
+function loadUserProfile(user){
+    userNameSpan.textContent = user.name;
+    walletSpan.textContent = user.wallet;  
+}
