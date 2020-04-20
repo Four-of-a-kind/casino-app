0998e2bbe2add8e3148ab0a0ae2569374fe1ba10
Jpendy <Jpendy256@gmail.com>
2020-04-20 16:06:20 -0700

    added data array for slots

---
+++ data/slots-data.js
@@ -0,0 +1,17 @@
+export const slotsData = [
+    {
+        id: 1,
+        value: 5,
+        image: 'dan.jpeg'
+    },
+    {
+        id: 2,
+        value: 10,
+        image: 'jake.jpeg'
+    },
+    {
+        id: 3,
+        value: 20,
+        image: 'jaime.jpeg'
+    },
+];
