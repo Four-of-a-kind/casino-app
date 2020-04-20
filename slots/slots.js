98ee84bbf2b7589862f4e5bbc00d087d3061f1dd
internetcharles <charlesodellsmith@gmail.com>
2020-04-20 16:40:27 -0700

    added generateRandom function and set initial images

--- slots/slots.js
+++ slots/slots.js
@@ -4,7 +4,8 @@ import { slotsData } from '../data/slots-data.js';
 const userNameSpan = document.getElementById('user-name-span');
 const walletSpan = document.getElementById('wallet-span');
 const spinSpan = document.getElementById('spin-span');
-
+const reel1Image = document.getElementById('reel-one-image');
+const reel2Image = document.getElementById('reel-two-image');
 const spinButton = document.getElementById('spin-button');
 
 const user = JSON.parse(localStorage.getItem('USER'));
@@ -15,9 +16,25 @@ spinSpan.textContent = spinCounter;
 
 loadUserProfile(user);
 
+// uses generate random function to set reel
+let reel1 = generateRandom(slotsData);
+let reel2 = generateRandom(slotsData);
+
+function checkResult(reel1, reel2) {
+    if (reel1.id === reel2.id) {
+
+    }
+}
 
 
 
+// generates a random object from the slots data
+function generateRandom(array) {
+    const indexNumber = Math.floor(Math.random() * 3);
+    const randomObject = array[indexNumber];
+
+    return randomObject;
+}
 
 // load user profile function
 function loadUserProfile(user){
